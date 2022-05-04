3bb061287720dc609b438b6acfaf0befcc06c453
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-05-04 14:06:11 +0200

    Added hover overlay effect to the projects section

--- src/Pojects.js
+++ src/Pojects.js
@@ -9,22 +9,63 @@ const Projects = () => {
         <div className="projectsSection" id="projectsSec">
             <h1 className="projectsHeading">Projects</h1>
             <div className="projects">
+
                 <div className="columns">
-                    <img src={Picture1} alt="Projects Done" />
+                    <div className="project">
+                        <div className="content-overlay"></div>
+                        <img src={Picture1} alt="Blog Project" className="projectImage"/>
+                        <div className="content-details fadeIn-top">
+                            <p>This is a short description</p>
+                        </div>
+                    </div>
+                    <p className="projectTitle">Blog Project</p>
+                </div>
+
+                <div className="columns">
+                    <div className="project">
+                        <div className="content-overlay"></div>
+                        <img src={Picture2} alt="Link Shortening Project" />
+                        <div className="content-details fadeIn-top">
+                            <p>This is a short description</p>
+                        </div>
+                    </div>
+                    <p className="projectTitle">Link Shortener</p>
+                </div>
+
+                <div className="columns">
+                    <div className="project">
+                        <div className="content-overlay"></div>
+                        <img src={Picture3} alt="To-Do List App" />
+                        <div className="content-details fadeIn-top">
+                            <p>This is a short description</p>
+                        </div>
+                    </div>
+                    <p className="projectTitle">To-Do List App</p>
+                </div>
+
+                <div className="columns">
+                    <div className="project">
+                        <div className="content-overlay"></div>
+                        <img src={Picture4} alt="CRUD App" />
+                        <div className="content-details fadeIn-top">
+                            <p>This is a short description</p>
+                        </div>
+                    </div>
+                    <p className="projectTitle">CRUD Application</p>
+                </div>
+
+                {/* <div className="columns">
+                    <img src={Picture2} alt="Link Shortening Project" />
                     <p className="projectDescription">Enter Project description here...</p>
                 </div>
                 <div className="columns">
-                    <img src={Picture2} alt="Projects Done" />
+                    <img src={Picture3} alt="To-Do List App" />
                     <p className="projectDescription">Enter Project description here...</p>
                 </div>
                 <div className="columns">
-                    <img src={Picture3} alt="Projects Done" />
+                    <img src={Picture4} alt="CRUD App" />
                     <p className="projectDescription">Enter Project description here...</p>
-                </div>
-                <div className="columns">
-                    <img src={Picture4} alt="Projects Done" />
-                    <p className="projectDescription">Enter Project description here...</p>
-                </div>
+                </div> */}
             </div>
         </div>
         
