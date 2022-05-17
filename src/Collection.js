1c3dd62128cbc7dc807750b33848bedb9995a362
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-05-17 11:11:37 +0200

    Updated new project display picture

--- src/Collection.js
+++ src/Collection.js
@@ -3,15 +3,14 @@ import Picture2 from "./Images/link-shortener.PNG";
 import Picture3 from "./Images/todolist.PNG";
 import Picture4 from "./Images/crudapp.PNG";
 import Picture5 from "./Images/sneaker-bot.PNG";
-import Picture6 from "./Images/weather-app.gif";
+import Picture6 from "./Images/weather-app.PNG";
 
 const collection = [
     {
         id: 1,
         picture: Picture1,
         title: "Blog Project",
-        description:
-        "This application is a blogging site, allowing users to add & delete their own custom blog entries",
+        description: "This application is a blogging site, allowing users to add & delete their own custom blog entries",
         stack: "Stack - HTML, CSS, JavaScript & ReactJS",
         link: "https://github.com/LeonMuguta/React-Blog-Project"
     },
@@ -19,8 +18,7 @@ const collection = [
         id: 2,
         picture: Picture2,
         title: "Link Sortener",
-        description:
-        "This application can by used by users to shorten long links, for cases where they may want to share a particular webpage. This was accomplished by using the bitly API.",
+        description: "This application can by used by users to shorten long links, for cases where they may want to share a particular webpage. This was accomplished by using the bitly API.",
         stack: "Stack - HTML, CSS, JavaScript & ReactJS",
         link: "https://github.com/LeonMuguta/Link-Shortener"
     },
@@ -28,8 +26,7 @@ const collection = [
         id: 3,
         picture: Picture3,
         title: "To-Do List App",
-        description:
-        "This is a simple to-do list app where users can input tasks they need to complete.",
+        description: "This is a simple to-do list app where users can input tasks they need to complete.",
         stack: "Stack - HTML, CSS & JavaScript",
         link: "https://github.com/LeonMuguta/ToDoList"
     },
